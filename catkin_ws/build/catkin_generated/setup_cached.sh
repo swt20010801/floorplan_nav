c70bab7164416e171356981a254cb5cfef8c981d
swt <swt20010801@163.com>
2023-10-20 13:54:35 +0800

    new

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/swt/Floorplan_nav/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/swt/Floorplan_nav/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PATH='/opt/ros/melodic/bin:/home/swt/anaconda3/bin:/home/swt/anaconda3/condabin:/home/swt/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH='/opt/ros/melodic/lib/pkgconfig:/usr/local/lib/pkgconfig'
+export PWD='/home/swt/Floorplan_nav/catkin_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/swt/Floorplan_nav/catkin_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH='/home/swt/Floorplan_nav/catkin_ws/src:/opt/ros/melodic/share'
